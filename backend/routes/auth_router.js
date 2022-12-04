7ff726556e1c300dade319aa1eef95786e993b48
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2022-12-04 19:18:28 +0200

    work

--- backend/routes/auth_router.js
+++ backend/routes/auth_router.js
@@ -1,12 +1,13 @@
 const Router = require('express');
 const router = new Router();
 const authController = require('../controllers/auth_controller');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.post('/password-reset', authController.password_reset);
 router.post('/password-reset/:token', authController.password_confirm);
-router.get('/refresh-token', authController.handleRefreshToken);
+router.get('/refresh-token', authMiddleware, authController.handleRefreshToken);
 
 module.exports = router;

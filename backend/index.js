5a77c4264a874e6162a63bb416f2bcdb2f3212d3
Жека <jeka20001980@gmail.com>
2022-12-04 09:31:11 +0200

    change gitignore and add errorHandler

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const bodyParser = require('body-parser');
 const router = require('./routes/index');
+const ErrorHandler = require('../../usof/backend/middleware/ErrorHandler');
 
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || "localhost";
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static")));
 app.use(cookieParser());
 app.use('/api', router);
+app.use(ErrorHandler);
 
 const start = async () => {
     try {
